699d8ccce7713e12b9c73b14b61c6236f5311936
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2017-03-09 13:02:56 -0500

    Fix sonar issues
    Signed-off-by: Olivier Thomann <Olivier_Thomann@ca.ibm.com>

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -18,6 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 6446827937200082683L;
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -26,7 +28,7 @@ public class HelloServlet extends HttpServlet {
 			response.setStatus(HttpServletResponse.SC_OK);
 			response.getWriter().println("<h1>Hello Servlet!</h1>");
 			response.getWriter().println("session=" + request.getSession(true).getId());
-		} catch(IOException e) {
+		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
